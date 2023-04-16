c816718cb97066177a695142ccf13ff396dbfea8
Zeeshan Ashraf <zeeshan.ashraf@dastgyr.com>
2023-04-16 23:53:29 +0500

    After Clustering LoadTest Report Added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,60 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cluster from 'cluster';
+import { cpus } from 'os';
+import * as net from "net";
+
+const clusterModule = cluster as unknown as cluster.Cluster;
+const numCPUs = cpus().length;
+const workers = {};
+
+let index = 0;
+const getWorkerIndex = () => {
+    index += 1;
+    if(index === numCPUs){
+        index = 0;
+    }
+    return index;
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  if (clusterModule.isPrimary) {
+    console.log(`
+             Master server started,proccess.pid:${process.pid},
+             number of cpus: ${numCPUs}
+    `);
+    for (let i = 0; i < numCPUs; i++) { 
+          workers[i] = clusterModule.fork();
+          workers[i].on('exit',(worker: cluster.Worker, code: number, signal: string) => {
+             console.log(`
+                      Worker with code: ${code} Restarting...
+             `);
+             console.log('worker %d died (%s). restarting...',
+             worker.process.pid, signal || code);
+             workers[i] = clusterModule.fork();
+          });
+    }
+
+    net.createServer({ pauseOnConnect: true },
+        (connection)=>{
+           const workerIndex = getWorkerIndex();
+           workers[workerIndex].send('sticky-session:connection', connection);
+    }).listen(3000);
+
+  } else {
+    const app = await NestFactory.create(AppModule);  
+    console.log(`
+       Worker server started, process.pid: ${process.pid}
+    `);
+    const server = await app.listen(0);
+    process.on('message', (message, connection: any) => {
+        if (message !== 'sticky-session:connection') {
+          return;
+        }
+       server.emit('connection', connection);
+       connection.resume();
+    });
+  }
 }
+
 bootstrap();
